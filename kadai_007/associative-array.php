3db38eff45cded515ec48f41d7bc10abaa2ef942
yoshida2025 <127272004+yoshida2023@users.noreply.github.com>
2025-02-24 21:05:50 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>課題2</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        $data = ['name' => 'onion', 'price' => 200, 'weight' => '160'];
+
+        // 連想配列の値を出力する
+        print_r($data);
+        ?>
+    </p>
+</body>
+
+</html>
